ae62154426d3b3d2a6d8056aed66ec6c3432a143
JeenaAanchal <jeenaaanchal@gmail.com>
2025-07-31 04:47:17 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+export default {
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish: ['Mulish', 'sans-serif'],
+      },
+      colors: {
+        deepBlue: "#02042a",
+        lightBlue:"#659BDF",
+        offWhite: "#F2F0EF",
+        lightGreen:"#61cea6",
+        darkBlue:"#0C1221",
+        blueGradient: "#000091",
+        lightBluish:"#f2f9fc",
+      },
+    },
+  },
+  plugins: [],
+}
